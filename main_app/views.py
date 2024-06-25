0c7b74dfb8227a400c53511aa5e74371b4ee2535
RachelG <rachelmarie.gangemi@gmail.com>
2024-06-25 19:38:43 -0400

    base css base template

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,12 @@ from django.shortcuts import render
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
 
-# Define the home view function
+# HOME
 def home(request):
     # Send a simple HTML response
-    return HttpResponse('<Hello, Beautiful>')
+    return HttpResponse('<h1>Hello, Beautiful</h1>')
+
+# SERVICES
+def services(request):
+    # Send a simple HTML response
+    return render(request, 'services.html')
