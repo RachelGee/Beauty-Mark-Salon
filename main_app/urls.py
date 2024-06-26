c0a04754a47265fb8e2c6a2af152a5b1b6ced6d2
RachelG <rachelmarie.gangemi@gmail.com>
2024-06-25 23:43:52 -0400

    services index and styling add images

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
     # Routes will be added here
     path('', views.home, name='home'),
-    path('services/', views.services, name='services'),
+    path('about/', views.about, name='about'),
+    path('services/', views.service_index, name='service-index'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -11,6 +11,29 @@ def home(request):
     return HttpResponse('<h1>Hello, Beautiful</h1>')
 
 # SERVICES
-def services(request):
+def service_index(request):
+    return render(request, 'services/index.html', {'services': services})
+
+class Service:
+    def __init__(self, type, description, cost):
+        self.type = type
+        self.description = description
+        self.cost = cost
+
+services = [
+    Service('Blowout', '', 40),
+    Service('Womens Cut & Style', '', 60),
+    Service('Womens Master Cut & Style', '', 65),
+    Service('Mens Cut', '', 35),
+    Service('Mens Wash & Scissor Cut', '', 40),
+    Service('Lengthy Master Color', '', 200),
+    Service('Buzz Master Color', '', 100),
+    Service('Fix Your Face', 'Standard makeup', 60),
+    Service('Face First', 'Master makeup', 90),
+]
+
+
+# ABOUT
+def about(request):
     # Send a simple HTML response
-    return render(request, 'services.html')
+    return render(request, 'about.html')
